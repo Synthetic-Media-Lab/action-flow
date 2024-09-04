b34d9d75274f529a7c4e7bbd5bb466cdab1aed67
Dawid Dahl <dawid.dahl@umain.com>
2024-09-04 06:56:40 +0200

    revert: tests commit

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,17 @@
 module.exports = {
-    extends: ["@commitlint/config-conventional"]
+    extends: ["@commitlint/config-conventional"],
+    rules: {
+        // Enforce type to be always lowercase
+        "type-case": [2, "always", "lower-case"],
+
+        // Allow scope to be optional, but if present, it must be lowercase
+        "scope-case": [2, "always", "lower-case"],
+        "scope-empty": [0], // 0 means this rule is turned off, making the scope optional
+
+        // Enforce subject to start with a lowercase letter
+        "subject-case": [2, "always", ["lower-case"]],
+
+        // Enforce that the subject does not end with a full stop
+        "subject-full-stop": [2, "never", "."]
+    }
 }
